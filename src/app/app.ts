ba552627927e9ae08a96328582c34b7d609307da
nyandwi <eric.nyandwi.didier@gmail.com>
2025-10-28 22:53:36 +0100

    delete element no use in import

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,8 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { CombineLatest_ } from './components/observables/combine-latest/combine-latest_.component';
 import {Header} from './components/observables/behavior-subject/header/header';
 import {Login} from './components/observables/behavior-subject/login/login';
-import {combineLatest} from 'rxjs';
+
 
 @Component({
   selector: 'app-root',
--- src/app/components/observables/behavior-subject/login/login.ts
+++ src/app/components/observables/behavior-subject/login/login.ts
@@ -1,6 +1,6 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {EtatConnexion} from '../../../../core/services/etat-connexion';
-import {Observable, Subscription} from 'rxjs';
+import { Subscription} from 'rxjs';
 
 
 @Component({
